7d467a418856a7813166496d61808fb27b7b970a
astoff <astoff@users.noreply.github.com>
2014-03-02 14:57:35 -0500

    Improved DMS parsing

--- README.md
+++ README.md
@@ -11,17 +11,18 @@ Dependencies:
 Usage
 -----
 
-If your csv file is reasonably well formatted and requires no coordinate system transformation, then `./csv2osm.py CSV_FILE.csv > OSM_FILE.osm`  should do the trick.  If you need more info, type `./csv2osm.py -h`.
+If your csv file is reasonably well formatted and requires no coordinate system transformation, then `./csv2osm.py CSV_FILE.csv OSM_FILE.osm`  should do the trick.  If you need more info, type `./csv2osm.py -h`.
 
     usage: csv2osm.py [-h] [-x FIELD_NAME] [-y FIELD_NAME] [-l LOCALE]
 		      [--proj4 PROJ4_STRING] [--sirgas2000 ZONE] [--sad69 ZONE]
 		      [--way]
-		      csv
+		      [infile] [outfile]
 
     Convert csv spreadsheets to osm format.
 
     positional arguments:
-      csv                   Input csv file
+      infile                Input csv file
+      outfile               Output osm file
 
     optional arguments:
       -h, --help            show this help message and exit
@@ -35,8 +36,8 @@ If your csv file is reasonably well formatted and requires no coordinate system 
       --sirgas2000 ZONE     Set input coordinate system to SIRGAS2000 in the given
 			    UTM zone
       --sad69 ZONE          Set input coordinate system to SAD69 in the given UTM
-			    zone
+			    zone. Use the special value 'll' for
+			    longitude/latitude coordinates.
       --way                 Create a way joining all nodes
-
 
 
--- csv2osm.py
+++ csv2osm.py
@@ -5,13 +5,14 @@ from xml.sax.saxutils import quoteattr
 import csv, argparse, sys, locale, re
 
 parser = argparse.ArgumentParser(description="Convert csv spreadsheets to osm format.")
-parser.add_argument("csv", help="Input csv file")
+parser.add_argument('infile', help="Input csv file", nargs='?', type=argparse.FileType('r'), default=sys.stdin)
+parser.add_argument('outfile', help="Output osm file", nargs='?', type=argparse.FileType('w'), default=sys.stdout)
 parser.add_argument("-x", "--lon", help="Name of the longitude/easting field", metavar="FIELD_NAME")
 parser.add_argument("-y", "--lat", help="Name of the latitude/northing field", metavar="FIELD_NAME")
 parser.add_argument("-l", "--locale", help="Locale used to convert coordinate fields to float")
 parser.add_argument("--proj4", help="Input coordinate system, given as a proj4 string", metavar="PROJ4_STRING")
 parser.add_argument("--sirgas2000", metavar="ZONE", help="Set input coordinate system to SIRGAS2000 in the given UTM zone")
-parser.add_argument("--sad69", metavar="ZONE", help="Set input coordinate system to SAD69 in the given UTM zone")
+parser.add_argument("--sad69", metavar="ZONE", help="Set input coordinate system to SAD69 in the given UTM zone. Use the special value 'll' for longitude/latitude coordinates.")
 parser.add_argument('--way', action='store_true', help="Create a way joining all nodes")
 args = parser.parse_args()
 
@@ -20,28 +21,41 @@ proj_out = Proj('+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs +towgs84=0,0,0
 if args.locale:
     locale.setlocale(locale.LC_NUMERIC, args.locale)
 
+# Projection conversions according to http://wiki.osgeo.org/wiki/Brazilian_Coordinate_Reference_Systems
 if args.proj4:
     proj_in = Proj(args.proj4)
 elif args.sirgas2000:
     proj_in = Proj('+proj=utm +zone={} +south +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs'.format(args.sirgas2000))
+elif args.sad69 == 'll':
+    proj_in = Proj('+proj=longlat +ellps=aust_SA +towgs84=-67.35,3.88,-38.22,0,0,0,0 +no_defs')
 elif args.sad69:
-    proj_in = Proj('+proj=utm +zone={} +south +ellps=aust_SA +towgs84=-57,1,-41,0,0,0,0 +units=m +no_defs'.format(args.sad69))
+    proj_in = Proj('+proj=utm +zone={} +south +ellps=aust_SA +towgs84=-67.35,3.88,-38.22,0,0,0,0 +units=m +no_defs'.format(args.sad69))
 else:
     proj_in = proj_out
 
-parse_dms = re.compile('(\d+)(?:°|º| deg)\s?(\d+)(?:\'|’| )\s?([\d,.]+)(?:"|”)?\s?([NSWE])')
+parse_dms = re.compile(r'''(?P<pre>[NSWE+-]?)\s?(?P<deg>\d+)(?:°|º|\s?deg)\s?(?P<min>\d+)(?:'|’| )\s?(?P<sec>[\d,.]+)(?:"|”)?\s?(?P<post>[NSWE]?)''')
 
 def parse_coord(c):
     try:
         return locale.atof(c)
     except:
         m = parse_dms.match(c)
-        return (locale.atof(m.group(1)) + locale.atof(m.group(2))/60 + locale.atof(m.group(3))/3600) * (-1 if m.group(4) in ['W', 'S'] else 1)
+        if m.group('post') and m.group('pre'):
+            h = None
+        elif m.group('post') in ['S', 'W'] or m.group('pre') in ['-', 'S', 'W']:
+            h = -1
+        elif m.group('post') in ['N', 'E'] or m.group('pre') in ['+', 'N', 'E']\
+             or not (m.group('post') and m.group('pre')):
+            h = 1
+        return h * (locale.atof(m.group('deg')) + locale.atof(m.group('min'))/60 + locale.atof(m.group('sec'))/3600)
 
-f=open(args.csv, 'r')
-dialect = csv.Sniffer().sniff(f.read(1024))
-f.seek(0)
-reader = csv.DictReader(f, dialect=dialect)
+if args.infile.seekable():
+    dialect = csv.Sniffer().sniff(args.infile.read(1024))
+    args.infile.seek(0)
+else:
+    dialect = csv.excel
+
+reader = csv.DictReader(args.infile, dialect=dialect)
 
 try:
     xkey = args.lon or [i for i in ["LONGITUDE", "longitude", "lon", "x", "e"] if i in reader.fieldnames][0]
@@ -49,7 +63,7 @@ try:
 except:
     sys.exit('Error: Coordinate fields not found in the csv file header.')
 
-print('<?xml version="1.0" encoding="UTF-8"?><osm version="0.6" generator="csv2osm">')
+print('<?xml version="1.0" encoding="UTF-8"?><osm version="0.6" generator="csv2osm">', file=args.outfile)
 
 i=0
 
@@ -60,16 +74,16 @@ for row in reader:
     except:
         print("Skipping node {}: couldn't parse coordinates".format(i), file=sys.stderr)
         continue
-    print('<node id="{}" lat="{}" lon="{}">'.format(i,lat, lon))
+    print('<node id="{}" lat="{}" lon="{}">'.format(i,lat, lon), file=args.outfile)
     for key in row:
         if row[key]:
-            print('<tag k="{}" v={}/>'.format(key, quoteattr(row[key])))
-    print('</node>')
+            print('<tag k="{}" v={}/>'.format(key, quoteattr(row[key])), file=args.outfile)
+    print('</node>', file=args.outfile)
 
 if args.way:
-    print('<way id="{}" changeset="false">'.format(i-1))
+    print('<way id="{}" changeset="false">'.format(i-1), file=args.outfile)
     for j in range(-1,i-1,-1):
-        print('<nd ref="{}"/>'.format(j))
-    print('</way>')
+        print('<nd ref="{}"/>'.format(j), file=args.outfile)
+    print('</way>', file=args.outfile)
 
-print('</osm>')
+print('</osm>', file=args.outfile)
